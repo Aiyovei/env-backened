31ad6627cde45874634a27389a3857e0f92d46e9
王兆君 <2112599720@qq.com>
2025-03-09 10:53:24 +0800

    增加了mybatis依赖和数据库配置信息

--- src/main/java/com/env/envbackend/EnvBackendApplication.java
+++ src/main/java/com/env/envbackend/EnvBackendApplication.java
@@ -1,9 +1,11 @@
 package com.env.envbackend;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.env.envbackend.mapper")
 public class EnvBackendApplication {
 
     public static void main(String[] args) {
